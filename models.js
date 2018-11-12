2de7d830c3dbf7393e948ecdf561dde6a360c3af
Adam Sisk <adamsisk91@gmail.com>
2018-11-12 16:56:01 -0600

    gary the gardener

--- models.js
+++ models.js
@@ -1,42 +1,46 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr', {
-  logging: true
-})
+  logging: false,
+});
 
 const Gardener = db.define('gardener', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   age: {
-    type: Sequelize.INTEGER
-  }
-})
+    type: Sequelize.INTEGER,
+  },
+});
 
 const Plot = db.define('plot', {
   size: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
   },
   shaded: {
-    type: Sequelize.BOOLEAN
-  }
-})
+    type: Sequelize.BOOLEAN,
+  },
+});
 
-const Vegetable = db.define('vegtable', {
+const Vegetable = db.define('vegetable', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   color: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
   plantedOn: {
-    type: Sequelize.DATE
-  }
-})
+    type: Sequelize.DATE,
+  },
+});
 
-Plot.belongsTo(Gardener)
-Gardener.hasOne(Plot)
+const Planting = db.define('planting');
 
-Vegetable.belongsToMany(Plot, { through: 'planting' })
-Plot.hasMany(Vegetable)
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
 
-module.exports = db
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
+
+Vegetable.belongsToMany(Plot, { through: 'planting' });
+Plot.belongsToMany(Vegetable, { through: 'planting' });
+
+module.exports = { Gardener, Plot, Vegetable, Planting, db };
--- seed.js
+++ seed.js
@@ -1,11 +1,46 @@
-const db = require('./models')
+const { Gardener, Plot, Vegetable, Planting, db } = require('./models');
 
-db.sync({ force: true })
+db.sync()
   .then(() => {
-    console.log('db connect success')
-    db.close()
+    console.log('db connect success');
+    db.close();
   })
   .catch(() => {
-    console.log('db connect fails')
-    db.close()
+    console.log('db connect fails');
+    db.close();
+  });
+
+// for these to work need to add 'where' statement
+// Vegetable.create({
+//   name: 'carrot',
+//   color: 'orange',
+// }).then(console.log('creation success'));
+// Vegetable.findOrCreate({
+//   name: 'beet',
+//   color: 'red',
+//   plantedOn: '11/12/18',
+// }).then(console.log('beet creation success'));
+// Vegetable.findOrCreate({
+//   name: 'potato',
+//   color: 'grayish',
+//   plantedOn: '03/12/1991',
+// }).then(console.log('potato creation success'));
+
+Gardener.create({
+  name: 'Gary the Gardener',
+  age: 98,
+  favoriteVegetableId: 3,
+})
+  .then(gary => {
+    return Plot.create({
+      size: 100000,
+      shaded: false,
+      gardenerId: 'Gary the Gardener'.id,
+    });
   })
+  .then(plot => {
+    return Planting.create({
+      plotId: plot.id,
+      vegetableId: 'Gary the Gardener'.favoriteVegetableId,
+    });
+  });
